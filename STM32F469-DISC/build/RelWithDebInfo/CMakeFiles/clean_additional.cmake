2db0d1a54ab8dfce9ade278219e296c65d623891
“Kevin <kpf5297@psu.edu>
2025-04-19 00:53:30 -0400

    Remove generated CMake install scripts and compile commands for the Bluepill UART project

--- STM32F469-DISC/build/RelWithDebInfo/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "RelWithDebInfo")
-  file(REMOVE_RECURSE
-  "STM32F469I-DISCO.map"
-  )
-endif()
